e09dcd409bd86feef76f636c069b1aad35d9cb6f
quangtranngoc <tranngocquang08012004@gmail.com>
2024-10-29 20:05:25 +0700

    Adjust API and add codes for VN

--- ds_code/function/utils.py
+++ ds_code/function/utils.py
@@ -3,7 +3,6 @@ import pandas as pd
 def regional_data_build(region_folder, src_folder, filename):
     keys = []
     city_dfs = []
-    index_len = 0
     
     df = pd.read_csv("data/region/" + region_folder + "/" + "cities.csv")
     for i in range(df.shape[0]):
@@ -16,16 +15,19 @@ def regional_data_build(region_folder, src_folder, filename):
     region_df = pd.concat(city_dfs, axis=1, keys=keys).dropna()
     region_df.to_csv("data/region/" + region_folder + "/" + filename)
     
+    
 def regional_weather_build(region_folder, filename="weather.csv"):
     regional_data_build(region_folder, "weather", filename)
+    
     
 def regional_aqi_build(region_folder, filename="air_quality.csv"):
     regional_data_build(region_folder, "air_quality", filename)
     
+    
 def read_regional_data(path_from_region):
     path = "data/region/" + path_from_region
     return pd.read_csv(path, index_col=0, header=[0, 1])
+
     
 if __name__ == "__main__":
-    #regional_weather_build("europe")
-    print(read_regional_data("europe/weather.csv"))
+    print(read_regional_data("vietnam/weather.csv"))
